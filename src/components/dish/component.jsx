f4b80811d2fb06ae4f6c0e10180b3d26cc069dd8
dr.alexey7 <adrozhdev@aimedpro.ru>
2023-11-26 20:59:30 +0300

    counter updated

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { Counter } from "../counter/component";
 
 export const Dish = ({ name }) => {
+  const [amount, setAmount] = useState(0);
   return (
     <div>
       <p>{name}</p>
-      <Counter min={0} max={5} defaultNumber={0}/>
+      <Counter
+        value={amount}
+        increment={() => {
+          setAmount((current) => current + 1);
+        }}
+        decrement={() => {
+          setAmount((current) => current - 1);
+        }}
+      />
     </div>
   );
 };
