a05e2b74a889bdeb6de86247f41e68f50dfb65f1
dr.alexey7 <adrozhdev@aimedpro.ru>
2023-12-06 22:00:35 +0300

    Theme with buttons added

--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "../contexts/theme/component";
 import { Footer } from "../footer/component";
 import { Header } from "../header/component";
 import styles from './styles.module.css'
 
 export const Layout = ({ children }) => {
   return (
+    <ThemeProvider>
     <div className={styles.root}>
       <Header />
       {children}
       <Footer />
     </div>
+    </ThemeProvider>
   );
 };
